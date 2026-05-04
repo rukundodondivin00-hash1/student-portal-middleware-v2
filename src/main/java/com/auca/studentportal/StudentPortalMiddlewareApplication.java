ea0b4bca41f134837d16f663e80a327ecbe0ccc2
rukundodondivin00-hash1 <rukundodondivin00@gmail.com>
2026-05-04 17:48:55 +0300

    Add missing import for @EnableAsync

--- src/main/java/com/auca/studentportal/StudentPortalMiddlewareApplication.java
+++ src/main/java/com/auca/studentportal/StudentPortalMiddlewareApplication.java
@@ -3,6 +3,7 @@ package com.auca.studentportal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import com.auca.studentportal.config.AucaApiProperties;
 
